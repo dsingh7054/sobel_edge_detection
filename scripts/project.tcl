22393f4b6393ac3186d27f74f4a3285ff8ffd763
dsingh7054 <devin.singh7054@gmail.com>
2026-06-20 23:06:02 -0400

    Finally got sdpram xci setup correctly in build script

--- scripts/project.tcl
+++ scripts/project.tcl
@@ -52,7 +52,6 @@ proc checkRequiredFiles { origin_dir} {
   set files [list \
 #  "[file normalize "$origin_dir/../example_project/Zybo-Z7-10-HDMI-hw.xpr/zybo-10-hw/archive_project_summary.txt"]"\
  "[file normalize "$origin_dir/../scripts/design_1.tcl"]"\
- "[file normalize "$origin_dir/../ip/sdpram_8bit_x_1920.xci"]"\
 #  "[file normalize "$origin_dir/../example_project/Zybo-Z7-10-HDMI-hw.xpr/zybo-10-hw/zybo-10-hw.srcs/utils_1/imports/synth_1/design_1_wrapper.dcp"]"\
   ]
   foreach ifile $files {
@@ -64,6 +63,7 @@ proc checkRequiredFiles { origin_dir} {
 
   set files [list \
  "[file normalize "$origin_dir/../sources/rgb2gray.vhd"]"\
+ "[file normalize "$origin_dir/../ip/sdpram_8bit_x_1920.xci"]"\
  "[file normalize "$origin_dir/../sources/ramArray.vhd"]"\
  "[file normalize "$origin_dir/../sources/ramArbitor.vhd"]"\
  "[file normalize "$origin_dir/../sources/sync_gen_pkg_p.vhd"]"\
@@ -236,6 +236,7 @@ if { $obj != {} } {
 set obj [get_filesets sources_1]
 set files [list \
  [file normalize "${origin_dir}/../sources/rgb2gray.vhd"] \
+ [file normalize "${origin_dir}/../ip/sdpram_8bit_x_1920.xci"] \
  [file normalize "${origin_dir}/../sources/ramArray.vhd"] \
  [file normalize "${origin_dir}/../sources/ramArbitor.vhd"] \
  [file normalize "${origin_dir}/../sources/sync_gen_pkg_p.vhd"] \
@@ -273,6 +274,15 @@ set file "$origin_dir/../sources/rgb2gray.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
+
+set file "$origin_dir/../ip/sdpram_8bit_x_1920.xci"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "generate_files_for_reference" -value "0" -objects $file_obj
+set_property -name "registered_with_manager" -value "1" -objects $file_obj
+if { ![get_property "is_locked" $file_obj] } {
+  set_property -name "synth_checkpoint_mode" -value "Singular" -objects $file_obj
+}
 
 set file "$origin_dir/../sources/ramArray.vhd"
 set file [file normalize $file]
